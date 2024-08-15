650c4a04da3e48963e0d4f753aed9e0934a93a49
Shahriar-Samir <shabusiness035@gmail.com>
2024-08-15 10:03:01 +0600

    add: submit handler

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 
 const Login = () => {
+    
+    const submitHandler = (e)=>{
+            e.preventDefault()
+            const form = e.target
+            const email = form.email.value
+            const password = form.password.value
+
+            console.log(email, password)
+    }
+
     return (
         <div className='w-full h-[100vh] flex justify-center items-center'>
              <div className='w-11/12 max-w-[400px]'>
                 <h1 className='text-center font-semibold text-3xl'>Login</h1>
-             <form className="card-body">
+             <form className="card-body" onSubmit={submitHandler}>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="email" placeholder="email" name='email' className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="password" placeholder="password" name='password' className="input input-bordered" required />
         </div>
         <div className="form-control mt-6">
           <button className="btn btn-primary">Login</button>

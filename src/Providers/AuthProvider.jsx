74ef2574f55b66118ad1b5c9feda7fe19d4d5b89
Shahriar-Samir <shabusiness035@gmail.com>
2024-08-15 10:09:23 +0600

    add: auth provider file

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+
+export const AuthContext = createContext(null)
+
+const AuthProvider = ({children}) => {
+    const userAuth = {}
+    return <AuthContext.Provider value={userAuth}>
+        {children}
+    </AuthContext.Provider>
+};
+
+export default AuthProvider;

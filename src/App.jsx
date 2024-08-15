0ac64b19b29ac826009b98035479823b5d08c8b5
Shahriar-Samir <shabusiness035@gmail.com>
2024-08-15 19:11:38 +0600

    add: header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import Header from './Components/Header';
+
 
 const App = () => {
-  return (
-    <div>
-      <Outlet/>
-    </div>
-  );
+    const location = useLocation()
+    if(location.pathname !== '/login' || location.pathname !== '/signup' ){
+      return (
+        <div>
+          <Header/>
+          <Outlet/>
+        </div>
+      );
+    }
+    return <div>
+              <Outlet/>
+          </div>
 };
 
 export default App;
--- src/Routes/AuthRoute.jsx
+++ src/Routes/AuthRoute.jsx
@@ -1,7 +1,7 @@
-import { Children, useContext } from "react";
+import { useContext } from "react";
 import Loading from "../Components/Loading";
 import { AuthContext } from "../Providers/AuthProvider";
-import { Navigate, useLocation, useNavigate } from "react-router-dom";
+import {  useLocation, useNavigate } from "react-router-dom";
 
 
 const AuthRoute = (children) => {
@@ -13,7 +13,7 @@ const AuthRoute = (children) => {
         return <Loading/>
     }
     if(user){
-    if(link.pathname === '/login' || link.pathname === 'signup'){
+    if(link.pathname === '/login' || link.pathname === '/signup'){
              return navigate(-1)
     }
 }
